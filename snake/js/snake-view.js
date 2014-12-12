0548d8e9277c808b494ec39e4299cda82d778273
jmorc <jeffcaves@gmail.com>
2014-12-12 10:35:19 -0800

    space bar pauses game

--- snake/js/snake-view.js
+++ snake/js/snake-view.js
@@ -6,24 +6,35 @@
     this.setBoard();
     $(window).on('keydown', this.handleKeydown.bind(this))
     this.board = new SnakeGame.Board();
+    this.pause = false;
     
     rootObj.setInterval(this.step.bind(this), 300);
   };
   
   View.prototype.handleKeydown = function(event) {
-    var keyDirs = { 37: 'L',
-                    38: 'U', 
-                    39: 'R', 
-                    40: 'D', 
-                  };
-    this.board.snake.turn(keyDirs[event.keyCode])
+    if (event.keyCode === 32) {
+      this.pauseToggle();
+    } else {
+      var keyDirs = { 37: 'L',
+                      38: 'U', 
+                      39: 'R', 
+                      40: 'D',
+                    };
+      this.board.snake.turn(keyDirs[event.keyCode]);
+    }
   };
   
+  View.prototype.pauseToggle = function() {
+    this.pause = (this.pause === false) ? true : false;
+  }
+  
   View.prototype.step = function() {
-    this.board.moveSnake();
-    this.board.renderBrowser();
-    this.board.maybeAddApples();
-    this.board.checkGameOver();
+    if (this.pause === false) {
+      this.board.moveSnake();
+      this.board.renderBrowser();
+      this.board.maybeAddApples();
+      this.board.checkGameOver();
+    }
   };
   
   View.prototype.setBoard = function() {
